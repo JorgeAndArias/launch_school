ad55871abe9e4a146d07aded4c9dc95777edb103
Jorge Andres Arias <jorgeariasgil@gmail.com>
2022-12-08 13:57:57 +1100

    add yml file and finished mortage calculator

--- RB101/lesson_2/mortage_calculator.rb
+++ RB101/lesson_2/mortage_calculator.rb
@@ -1,68 +1,104 @@
-# goal: Build a loan calcultor that takes 3 pieces of information and returns the monthly payment
+=begin
+goal: Build a loan calcultor that takes 3 pieces of information
+and returns the monthly payment
 
-# Loan ammount, annual percentage rate, loan duration
+Loan ammount, annual percentage rate, loan duration
 
-# get input from the user for the loan ammount and store it in a variable called loan_ammount
-# get input from the user for the annual percentage rate and store it in a variable called annual_percentage_rate
-# get input from the user for the loan duration and store it in a variable called loan_duration (years)
+get input from the user for the loan ammount, annual percentage rate and
+loan duration and store them variables
 
-# convert annual_percentage_rate to monthly
-# convert loan_duration to months
+convert rate to monthly
+ask if duration is in years or months and transform years to months
 
-# use formula to calculate the monthly payment and print it for the user
+use formula to calculate the monthly payment and print it for the user
+=end
+
+require 'yaml'
+
+MONTHS_YEAR = 12
+MESSAGES = YAML.load_file('mort_calc_messages.yml')
+# Clear the console before executing the program
+puts `clear`
 
 def prompt(message)
   puts "=> #{message}"
 end
 
-def integer?(num)
+def positive_integer?(num)
   num.to_i.to_s == num && num.to_i.positive?
 end
 
-def float?(num)
+def positive_float?(num)
   num.to_f.to_s == num && num.to_f.positive?
 end
 
 def valid_number?(num)
-  (integer?(num) || float?(num))
+  (positive_integer?(num) || positive_float?(num))
 end
 
-prompt("Welcome to the loan calculator")
+def calc_month_pay(loan_amt, ann_perc_rate, loan_dur)
+  total =
+    loan_amt.to_i * (((ann_perc_rate.to_f / 100) / MONTHS_YEAR) /
+    (1 - (1 + ((ann_perc_rate.to_f / 100) / MONTHS_YEAR))**(-(loan_dur))))
+  total.round(2)
+end
 
-loan_ammount = nil
+puts MESSAGES['welcome']
 
 loop do
-  prompt("Please enter the loan ammount:")
-  loan_ammount = gets.chomp
+  loan_ammount = nil
 
-  break if valid_number?(loan_ammount)
+  prompt(MESSAGES['loan_ammount'])
 
-  prompt("Please enter a valid loan ammount")
+  loop do
+    loan_ammount = gets.chomp
+    break if valid_number?(loan_ammount)
+    prompt(MESSAGES['invalid_loan_ammount'])
+  end
+
+  ann_percent_rate = nil
+
+  prompt(MESSAGES['ann_percent_rate'])
+
+  loop do
+    ann_percent_rate = gets.chomp
+    break if valid_number?(ann_percent_rate)
+    prompt(MESSAGES['invalid_ann_percent_rate'])
+  end
+
+  time_duration = nil
+
+  prompt(MESSAGES['time_of_duration'])
+
+  loop do
+    time_duration = gets.chomp
+    break if time_duration.downcase == 'm' || time_duration.downcase == 'y'
+    prompt(MESSAGES['invalid_time_of_duration'])
+  end
+
+  loan_duration = nil
+
+  prompt(MESSAGES['loan_duration'])
+
+  loop do
+    loan_duration = gets.chomp
+    if valid_number?(loan_duration) && time_duration.downcase == 'm'
+      loan_duration = loan_duration.to_i
+      break
+    elsif valid_number?(loan_duration) && time_duration.downcase == 'y'
+      loan_duration = loan_duration.to_i * MONTHS_YEAR
+      break
+    end
+    prompt(MESSAGES['invalid_loan_duration'])
+  end
+
+  monthly_pay = calc_month_pay(loan_ammount, ann_percent_rate, loan_duration)
+
+  prompt(format(MESSAGES['monthly_pay'], monthly_pay: monthly_pay))
+
+  prompt(MESSAGES['again'])
+  again_answer = gets.chomp
+  break unless again_answer.downcase().start_with?('y')
 end
 
-ann_percent_rate = nil
-
-loop do
-  prompt("Please enter the annual percentage rate:")
-  ann_percent_rate = gets.chomp
-
-  break if valid_number?(ann_percent_rate)
-
-  prompt("Please enter a valid annual percentage rate")
-end
-
-loan_duration = nil
-
-loop do
-  prompt("Please enter the loan duration (years):")
-  loan_duration = gets.chomp
-
-  break if valid_number?(loan_duration)
-
-  prompt("Please enter a valid loan_duration")
-end
-
-
-monthly_pay = loan_ammount.to_i * (((ann_percent_rate.to_f / 100) / 12) / (1 - (1 + ((ann_percent_rate.to_f / 100) / 12))**(-(loan_duration.to_i * 12))))
-
-p monthly_pay.round(2)
+puts MESSAGES['thank_you']
