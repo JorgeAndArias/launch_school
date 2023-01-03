b91620e072b86e388f0a08199020f40d3545a122
Jorge Andres Arias <jorgeariasgil@gmail.com>
2023-01-04 10:30:24 +1100

    easy 1 - repeat yourself

---
+++ RB101/RB101_RB109_small_problems/easy_1/repeat_yourself.rb
@@ -0,0 +1,60 @@
+=begin
+PROBLEM: Repeat yourself
+
+Write a method that takes two arguments, a string and a positive integer, and prints the string as many times as the integer indicates.
+
+Example:
+
+repeat('Hello', 3)
+Output:
+
+Hello
+Hello
+Hello
+
+Rules:
+- Method should take 2 arguments (string and an integer)
+- The integer of the second argument must be positive
+- The method should print the argument string as many times as the second argument
+
+EXAMPLES
+repeat('Hello', 3)
+Output:
+
+Hello
+Hello
+Hello
+
+repeat('HI', -1)
+Output:
+
+Second argument must be a positive integer (grater than zero)
+
+DATA STRUCTURES
+strings
+
+ALGORITHM
+- create a method called repeat that takes 2 arguments (string, repetion_times)
+- check if the argument times is a positive integer
+- if it is not a positive integer display a message saying "Second argument must be a positive integer (grater than zero)"
+- display the string argument as many times as the argument times
+=end
+
+def repeat(string, repetion_times)
+  puts " Second argument must be a positive integer (grater than zero)" unless repetion_times.positive?
+
+  repetion_times.times do
+    puts string
+  end
+
+end
+
+repeat('Hello', 3)
+
+# Hello
+# Hello
+# Hello
+
+# repeat('HI', -1)
+
+# Second argument must be a positive integer (grater than zero)
