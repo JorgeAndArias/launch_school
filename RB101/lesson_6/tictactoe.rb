64e6a2384239d5433624474a88baa4825754a897
Jorge Andres Arias <jorgeariasgil@gmail.com>
2023-01-11 10:33:29 +1100

    Add step 5: Play again

--- RB101/lesson_6/tictactoe.rb
+++ RB101/lesson_6/tictactoe.rb
@@ -4,13 +4,13 @@ INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
 
-
 def prompt(text)
   puts "=> #{text}"
 end
 
 def display_board(brd)
   system 'clear'
+  puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}"
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -79,22 +79,30 @@ def detect_winner(brd)
   nil
 end
 
-board = initialize_board
-
 loop do
+  board = initialize_board
+
+  loop do
+    display_board(board)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    comouter_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
   display_board(board)
 
-  player_places_piece!(board)
-  break if someone_won?(board) || board_full?(board)
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
 
-  comouter_places_piece!(board)
-  break if someone_won?(board) || board_full?(board)
+  prompt "Play again? (y or n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
 
-display_board(board)
-
-if someone_won?(board)
-  prompt "#{detect_winner(board)} won!"
-else
-  prompt "It's a tie!"
-end
+prompt "Thanks for playing Tic Tac Toe! Good bye!"
